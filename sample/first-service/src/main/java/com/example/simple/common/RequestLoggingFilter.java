70a4e28772b67d5fbb4d417462d98484c6151ac4
nanbingjun <nanbingjun@gmail.com>
2020-04-18 17:10:11 +0900

    upd

--- sample/first-service/src/main/java/com/example/simple/common/RequestLoggingFilter.java
+++ sample/first-service/src/main/java/com/example/simple/common/RequestLoggingFilter.java
@@ -10,34 +10,36 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
-
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
-public class LoggingFilter implements Filter {
-	
+public class RequestLoggingFilter implements Filter {
+
 	   @Override
 	    public void init(FilterConfig filterConfig) throws ServletException {
-	        System.out.println("### LoggingFilter#init() ###");
+	        log.info("### RequestLoggingFilter#init() ###");
 	    }
 
 	    @Override
 	    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 	    	writeHttpHeaders((HttpServletRequest) request);
-	    	chain.doFilter(request, response);	    	
-	    	System.out.println("# HTTP Request Compleate. #");
+	    	chain.doFilter(request, response);
+	    	log.info("# HTTP Request Compleate. #");
 	    }
-	    
+
 	    private void writeHttpHeaders(HttpServletRequest request) {
-	    	System.out.println(" ");
-	    	System.out.println("# LoggingFilter HTTP Headers [BEGIN] #");
+			log.info(" ");
+	    	log.info("# Http Request Path: {} #", request.getPathInfo());
+	    	log.info("# Http Request Headers [BEGIN] #");
 	  	  	Enumeration headerNames = request.getHeaderNames();
 	  	  	while	(headerNames.hasMoreElements()) {
 	  	  		String key = (String) headerNames.nextElement();
-	  	  		System.out.println("# " + key + " = " + request.getHeader(key));
+	  	  		log.info("# {} = {}", key, request.getHeader(key));
 	  	  	}
-	  	  	System.out.println("# LoggingFilter HTTP Headers [END] #");
-	  		System.out.println(" ");
+	  	  	log.info("#Http Request Headers [END] #");
+			log.info(" ");
 	    }
-	    
+
 }
